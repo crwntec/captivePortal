1b53b9fec1ff2faf5925845f4d3c3e5b85d96f37
Neuron Electronics <develop.neuron@gmail.com>
2023-07-21 18:06:37 +0200

    exec now works

--- portal.js
+++ portal.js
@@ -1,22 +1,29 @@
 const bodyParser = require('body-parser');
 const { exec } = require('child_process');
-const express = require('express')
+const express = require('express');
 
 const PORT = 8080;
 
-var app = express(express)
+var app = express(express);
 app.use(bodyParser.urlencoded({ extended: true }));
 let indexFile = "";
 
 app.get("/", (req,res)=>{
-    res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + "/index.html");
 })
 app.post("/accept", (req,res)=>{
     if(req.body.mtd="aa38b8bcaf0de4b40cf2769fb2006fd4"){
-        exec('echo "The \\$HOME variable is $HOME"');
-        res.sendStatus(200);
+        exec('echo test',(err,stdout,stderr)=>{
+            if (err){
+                console.log(err);
+                res.sendStatus(500);
+                return;
+            }
+            console.log(stdout)
+            res.sendStatus(200);
+        });
     } else {
-        res.sendStatus(403)
+        res.sendStatus(403);
     }
 })
 app.listen(PORT)
