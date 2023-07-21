186bd09c12d5184fc21388c9af801c8a3fb90948
Neuron Electronics <develop.neuron@gmail.com>
2023-07-21 18:00:24 +0200

    initial commit

---
+++ portal.js
@@ -0,0 +1,22 @@
+const bodyParser = require('body-parser');
+const { exec } = require('child_process');
+const express = require('express')
+
+const PORT = 8080;
+
+var app = express(express)
+app.use(bodyParser.urlencoded({ extended: true }));
+let indexFile = "";
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/index.html")
+})
+app.post("/accept", (req,res)=>{
+    if(req.body.mtd="aa38b8bcaf0de4b40cf2769fb2006fd4"){
+        exec('echo "The \\$HOME variable is $HOME"');
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(403)
+    }
+})
+app.listen(PORT)
